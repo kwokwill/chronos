aea32a2351e03d4be416c91ec8a14d5e5c503973
kwokwill <william_3410@hotmail.com>
2019-11-14 10:23:45 -0800

    Changing id and var

--- js/fbtoform.js
+++ js/fbtoform.js
@@ -1,75 +1,69 @@
-const cafeList = document.querySelector('#cafe-list');
-const form = document.querySelector('#add-cafe-form');
+const workList = document.querySelector('#work-list');
+const form = document.querySelector('#add-work-form');
 
-// create element & render cafe
-function renderCafe(doc){
+// create element & render workList
+function renderWork(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
-    let city = document.createElement('span');
+    let dueDate = document.createElement('span');
     let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
-    city.textContent = doc.data().city;
+    dueDate.textContent = doc.data().dueDate;
     cross.textContent = 'x';
 
     li.appendChild(name);
-    li.appendChild(city);
+    li.appendChild(dueDate);
     li.appendChild(cross);
 
-    cafeList.appendChild(li);
+    workList.appendChild(li);
 
     // deleting data
     cross.addEventListener('click', (e) => {
         e.stopPropagation();
         let id = e.target.parentElement.getAttribute('data-id');
-        db.collection('cafes').doc(id).delete();
+        db.collection('work').doc(id).delete();
     });
 }
 
-// getting data
-// db.collection('cafes').orderBy('city').get().then(snapshot => {
-    // snapshot.docs.forEach(doc => {
-        // renderCafe(doc);
-    // });
-// });
-
-// saving data
+// saving data and adding timestamp to allow work to be ordered
 // data saved and is submitted with name/city/createdAt (what time)
 form.addEventListener('submit', (e) => {
     
     e.preventDefault();
     
-    db.collection('cafes').add({
+    db.collection('work').add({
         name: form.name.value,
-        city: form.city.value,
+        dueDate: form.dueDate.value,
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
         
     });
     form.name.value = '';
-    form.city.value = '';
+    form.dueDate.value = '';
     
 });
 
-// allowing submission time for add-cafe-form and then rendering afterwards to bring newest li up top!!!!
+// allowing submission time for add-work-form and then rendering afterwards to bring newest li up top!!!!
 document.getElementById("myButton").addEventListener("click",function () {
     var timeoutID = window.setTimeout(function () {
         location.reload();
     }, 180);
   });
 
+
 // real-time listener
-db.collection("cafes").orderBy("createdAt", "desc").onSnapshot(snapshot => {
+db.collection("work").orderBy("createdAt", "desc").onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
         console.log(change.doc.data());
         
         if(change.type == 'added'){
-            renderCafe(change.doc);
+            renderWork(change.doc);
             
         } else if (change.type == 'removed'){
-            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
-            cafeList.removeChild(li);
+            let li = workList.querySelector('[data-id=' + change.doc.id + ']');
+            workList.removeChild(li);
         }
     });
     
@@ -77,6 +71,7 @@ db.collection("cafes").orderBy("createdAt", "desc").onSnapshot(snapshot => {
 
 var date = new Date();
 console.log(date);
+
 //updating records (console demo)
 // db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').update({
     // name: 'mario world'
