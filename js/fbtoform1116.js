601a7740ee4f994597203460a9f8647439f6678d
Adam <adamkay123@live.com>
2019-11-21 12:32:12 -0800

    more design/ gave comp1116 proper functionality

---
+++ js/fbtoform1116.js
@@ -0,0 +1,87 @@
+const workList = document.querySelector('#COMP1116-list');
+const form = document.querySelector('#add-1116-form');
+
+// create element & render workList
+function renderWork(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let dueDate = document.createElement('span');
+    let description = document.createElement('span');
+    let cross = document.createElement('div');
+    
+    var stringMonth = doc.data().dueDate +'';
+    var res = stringMonth.split("-"); // turn the date into a list format (Split by / if needed)
+    var months = ["Jan", "Feb", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"]; 
+    
+    dueDate.textContent = (months[res[1]-1] + " " + [res[2]] + ", " + [res[0]]);
+    
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    description.textContent = doc.data().description;
+    cross.textContent = 'x';
+
+    li.appendChild(name);
+    li.appendChild(description);
+    li.appendChild(dueDate);
+    li.appendChild(cross);
+    
+    workList.appendChild(li);
+
+    // deleting data
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('COMP1116').doc(id).delete();
+    });
+}
+
+// saving data and adding timestamp to allow work to be ordered
+// data saved and is submitted with name/city/createdAt (what time)
+form.addEventListener('submit', (e) => {
+    
+    e.preventDefault();
+    
+    db.collection('COMP1116').add({
+        name: form.name.value,
+        dueDate: form.dueDate.value,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        description: form.description.value,
+        
+    });
+    form.name.value = '';
+    form.dueDate.value = '';
+    form.description.value = '';
+    var timeoutID = window.setTimeout(function () {
+        location.reload();
+    }, 300);
+    
+});
+//^^^^^^^^^^^^^^^^^^^
+// allowing submission time for add-work-form and then rendering afterwards to bring newest li up top!!!!
+//document.getElementById("myButton").addEventListener("click",function () {
+//    var timeoutID = window.setTimeout(function () {
+//        location.reload();
+//    }, 180);
+//  });
+
+
+// real-time listener
+db.collection("COMP1116").orderBy("createdAt", "desc").onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        
+        if(change.type == 'added'){
+            renderWork(change.doc);
+            
+        } else if (change.type == 'removed'){
+            let li = workList.querySelector('[data-id=' + change.doc.id + ']');
+            workList.removeChild(li);
+        }
+    });
+    
+});
+
+var date = new Date();
+console.log(date);
