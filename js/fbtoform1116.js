ba26b89f6623ac8f34c1ea9612b6211893d54030
Adam <adamkay123@live.com>
2019-11-26 17:12:48 -0800

    updating posting form to show selected courses due dates

--- js/fbtoform1116.js
+++ js/fbtoform1116.js
@@ -2,29 +2,31 @@ const workList = document.querySelector('#COMP1116-list');
 const form = document.querySelector('#add-1116-form');
 
 // create element & render workList
-function renderWork(doc){
+function renderWork(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let dueDate = document.createElement('span');
     let description = document.createElement('span');
     let cross = document.createElement('div');
-    var stringMonth = doc.data().dueDate +'';
-    
+    var stringMonth = doc.data().dueDate + '';
+
     var res = stringMonth.split("-"); // turn the date into a list format (Split by / if needed)
-    var months = ["Jan", "Feb", "March", "April", "May", "June", "July", 
-    "August", "September", "October", "November", "December"]; 
-    
-    dueDate.textContent = (months[res[1]-1] + " " + [res[2]] + ", " + [res[0]]);
-    
+    var months = ["Jan", "Feb", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+    ];
+
+    dueDate.textContent = (months[res[1] - 1] + " " + [res[2]] + ", " + [res[0]]);
+
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     description.textContent = doc.data().description;
     cross.textContent = 'x';
+    li.className += 'listeroos';
     li.appendChild(name);
     li.appendChild(description);
     li.appendChild(dueDate);
     li.appendChild(cross);
-    
+
     workList.appendChild(li);
 
     // deleting data
@@ -39,7 +41,7 @@ function renderWork(doc){
 // data saved and is submitted with name/city/createdAt (what time)
 form.addEventListener('submit', (e) => {
     var cs = document.getElementById("wow")
-    var strUser = cs.options[cs.selectedIndex].value;    
+    var strUser = cs.options[cs.selectedIndex].value;
     e.preventDefault();
     console.log(strUser);
     db.collection(strUser).add({
@@ -47,7 +49,7 @@ form.addEventListener('submit', (e) => {
         dueDate: form.dueDate.value,
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
         description: form.description.value,
-        
+
     });
     form.name.value = '';
     form.dueDate.value = '';
@@ -55,7 +57,7 @@ form.addEventListener('submit', (e) => {
     var timeoutID = window.setTimeout(function () {
         // location.reload();
     }, 1500);
-    
+
 });
 //^^^^^^^^^^^^^^^^^^^
 // allowing submission time for add-work-form and then rendering afterwards to bring newest li up top!!!!
@@ -65,23 +67,33 @@ form.addEventListener('submit', (e) => {
 //    }, 180);
 //  });
 
-
+function clearList() {
+    var lists = document.getElementsByClassName('listeroos');
+    for(var index = 0; index < lists.length; index++) {
+        lists[index].style.display = "none";
+    }
+}
+var select = document.getElementById('wow');
+select.addEventListener('change', logValue, false);
 // real-time listener
-db.collection("COMP1116").orderBy("createdAt", "desc").onSnapshot(snapshot => {
-    let changes = snapshot.docChanges();
-    changes.forEach(change => {
-        console.log(change.doc.data());
-        
-        if(change.type == 'added'){
-            renderWork(change.doc);
-            
-        } else if (change.type == 'removed'){
-            let li = workList.querySelector('[data-id=' + change.doc.id + ']');
-            workList.removeChild(li);
-        }
-    });
-    
-});
+function logValue() {
+    clearList();
+    db.collection(this.value).orderBy("createdAt", "desc").onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
 
+        changes.forEach(change => {
+            console.log(change.doc.data());
+
+            if (change.type == 'added') {
+                renderWork(change.doc);
+
+            } else if (change.type == 'removed') {
+                let li = workList.querySelector('[data-id=' + change.doc.id + ']');
+                workList.removeChild(li);
+            }
+        });
+
+    });
+}
 var date = new Date();
 console.log(date);
